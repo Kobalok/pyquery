5cab5fea45c9719f3cf96af3cfa8093de1dfbbbc
Putrin Andrey <kobalok@gmail.com>
2024-10-15 09:11:30 +0300

    123

--- pyquery.py
+++ pyquery.py
@@ -10,19 +10,19 @@ DATABASE = 'master'
 USERNAME = 'sa'
 PASSWORD = 'efarma2'
 
-with open('out.csv', 'w', encoding='utf8') as csvout:
+with open('result.csv', 'w', encoding='utf8') as csvout:
         spamwriter = csv.writer(csvout, delimiter=';')
         
-        with open('in.csv', 'r', encoding='utf8') as csvfile:
+        with open('out.csv', 'r', encoding='utf8') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=';')
             for row in spamreader:
-                mserver=row[0]+SERVER 
+                mserver=row[0]#+SERVER 
               
                 connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={mserver};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
                 try:
                     conn = pyodbc.connect(connectionString) 
                     SQL_QUERY = """
-                    select name from sys.databases
+                               select name from sys.databases
                     """
                     cursor = conn.cursor()
                     cursor.execute(SQL_QUERY)
@@ -32,24 +32,97 @@ with open('out.csv', 'w', encoding='utf8') as csvout:
                                 mdb=r[0]
                                 break
                     cursor.close()
-                    SQL_QUERY = f'USE {mdb};'
-                    cursor = conn.cursor()
-                    cursor.execute(SQL_QUERY)
-                    cursor.close()
-                    SQL_QUERY = """
-                update KIZ_INVENTORY_ITEM_MDLP_CSV
-set SOLUTION='Отправить 552', SCHEME=552, ID_CHEQUE=NULL
-where ID_KIZ_INVENTORY_GLOBAL=(select top 1 ID_KIZ_INVENTORY_GLOBAL from KIZ_INVENTORY
-order by DOC_DATE desc) and ID_CHEQUE is not NULL
-and ID_CHEQUE in(select ID_CHEQUE from CHEQUE where DATE_CHEQUE<cast(cast(getdate() as date)as datetime)-29)
+#                    SQL_QUERY = f'USE {mdb};'
+ #                   cursor = conn.cursor()
+  #                  cursor.execute(SQL_QUERY)
+   #                 cursor.close()
+                    SQL_QUERY = f"""USE {mdb};
+                               SET QUOTED_IDENTIFIER ON;
+if object_id('tempdb..#temp') is not null  drop table #temp;
+select distinct k.ID_KIZ_GLOBAL, ki.ID_KIZ_ITEM_GLOBAL, km.REQUEST_ID, km.DOCUMENT_OUT_ID,km.ID_OPERATION, ki.ID_DOCUMENT 
+into #temp
+from kiz k
+join kiz_item ki on k.id_kiz_global=ki.id_kiz_global
+join kiz_move km on ki.id_kiz_item_global=km.id_kiz_item_global
+join KIZ_2_DOCUMENT_ITEM k2 on k.ID_KIZ_GLOBAL=k2.ID_KIZ_GLOBAL
+join LOT l on k2.ID_DOCUMENT_ITEM_ADD=l.ID_DOCUMENT_ITEM_ADD
+where (k.state='HOLD' or k.state='PROC' or k.state='EXIT') and  km.ID_ERROR in(-2,-1,0,11)
+and l.QUANTITY_REM+ l.QUANTITY_RES>0
+--and km.STATE='OK1'
+--and k2.DATE_MODIFIED<getdate()-0.083
+and l.INVOICE_NUM in(
 
-                     """
+--'52263/ПН-00011530',
+'419/ПН-00078488'
+);
+
+
+if (select  count(*)  from  #temp)=0 return;
+
+DECLARE @KIZ1 as uniqueidentifier;
+DECLARE @KIZ2 as uniqueidentifier;
+DECLARE @KIZ3 as uniqueidentifier;
+DECLARE @KIZ4 as uniqueidentifier;
+DECLARE @KIZ5 as uniqueidentifier;
+DECLARE @KIZ6 as uniqueidentifier;
+
+DECLARE db_cursor CURSOR FOR
+
+
+select  ID_KIZ_GLOBAL, ID_KIZ_ITEM_GLOBAL, REQUEST_ID, DOCUMENT_OUT_ID, ID_OPERATION, ID_DOCUMENT   from  #temp;
+OPEN db_cursor;
+
+
+FETCH NEXT FROM db_cursor INTO @KIZ1, @KIZ2, @KIZ3, @KIZ4, @KIZ5, @KIZ6 ; 
+WHILE (@@FETCH_STATUS <> -1) BEGIN
+
+
+      	
+		update KIZ
+			set STATE='PROC'
+		WHERE ID_KIZ_GLOBAL=@KIZ1 AND STATE='HOLD';
+
+		update KIZ_ITEM
+			set IS_READY=1
+		WHERE ID_KIZ_ITEM_GLOBAL=@KIZ2 AND IS_READY=0;
+INSERT INTO KIZ_MOVE
+           (ID_KIZ_MOVE_GLOBAL
+           ,ID_KIZ_ITEM_GLOBAL
+           ,REQUEST_ID
+           ,DOCUMENT_OUT_ID
+           ,OP_DATE
+           ,OP_TYPE
+           ,ID_ERROR
+           ,ERROR
+           ,STATE
+           ,ID_STATE
+           ,ID_OPERATION)
+     VALUES
+           (newid()
+           ,@KIZ2
+           ,@KIZ3
+           ,@KIZ4
+           ,getdate()
+           ,'InBwReport'
+           ,0
+           ,''
+           ,'OK'
+           ,7
+           ,@KIZ5);
+  
+  FETCH NEXT FROM db_cursor  
+   INTO @KIZ1, @KIZ2, @KIZ3, @KIZ4, @KIZ5, @KIZ6;
+END;
+CLOSE db_cursor;
+DEALLOCATE db_cursor;
+"""
                     cursor = conn.cursor()
                     cursor.execute(SQL_QUERY)
+                    conn.commit()
                     while cursor.nextset():   # NB: This always skips the first resultset
                         try:
-                            results = cursor.fetchall()
-                            break
+                           results = cursor.fetchall()
+                           break
                         except pyodbc.ProgrammingError:
                             continue
                     spamwriter.writerow([row,"Запрос выполнен без ошибок!!"])
